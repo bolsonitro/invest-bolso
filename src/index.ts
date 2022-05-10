4a31c5f07616eaa905ba04f8c2ff02476dd296cd
bolsonitro <bolsonitro_bitcoiner@protonmail.com>
2022-05-10 20:58:48 -0300

    added endpoints mstr and bolsonaro

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
 import express from "express";
+import axios from "axios";
+import { Ifinance, IBolsonaro } from "./interfaces";
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "OK" });
+});
+
+app.get("/mstr", (req, res) => {
+  axios
+    .get<Ifinance>(
+      "https://query1.finance.yahoo.com/v7/finance/quote?symbols=MSTR"
+    )
+    .then(({ data }) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      if (axios.isAxiosError(err)) res.status(500).json(err.message);
+      else res.status(500);
+    });
+});
+
+app.get("/bolsonaro", (req, res) => {
+  axios
+    .get<IBolsonaro>("https://ftx.com/api/markets/BOLSONARO2022")
+    .then(({ data }) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      if (axios.isAxiosError(err)) res.status(500).json(err.message);
+      else res.status(500);
+    });
 });
 
 export default app;
---
+++ src/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface Ifinance {
+  quoteResponse: {
+    result: [any];
+    error: any;
+  };
+}
+
+export interface IBolsonaro {
+    sucess: any,
+    result: any
+}
