b4c1ed66d149531f5e1b319d964794dd9162655e
bolsonitro <bolsonitro_bitcoiner@protonmail.com>
2022-05-14 20:27:02 -0300

    Changes
    Typing in req and res
    Added more interfaces
    Added /brz endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import { Ifinance, Iftx } from "./interfaces";
+import { Request, Response } from "express";
 const app = express();
 
 app.use(cors({ origin: "*" }));
@@ -10,7 +11,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
-app.get("/mstr", (req, res) => {
+app.get("/mstr", (req: Request, res: Response) => {
   axios
     .get<Ifinance>(
       "https://query1.finance.yahoo.com/v7/finance/quote?symbols=MSTR"
@@ -24,7 +25,7 @@ app.get("/mstr", (req, res) => {
     });
 });
 
-app.get("/bolsonaro", (req, res) => {
+app.get("/bolsonaro", (req: Request, res: Response) => {
   axios
     .get<Iftx>("https://ftx.com/api/markets/BOLSONARO2022")
     .then(({ data }) => {
@@ -36,7 +37,7 @@ app.get("/bolsonaro", (req, res) => {
     });
 });
 
-app.get("/bitcoin", (req, res) => {
+app.get("/bitcoin", (req: Request, res: Response) => {
   axios
     .get<Iftx>("https://ftx.com/api/markets/BTC/USD")
     .then(({ data }) => {
@@ -47,5 +48,17 @@ app.get("/bitcoin", (req, res) => {
       else res.status(500);
     });
 });
+
+app.get("/brz", (req: Request, res: Response) => {
+  axios
+    .get<Iftx>("https://ftx.com/api/markets/BRZ/USD")
+    .then(({ data }) => {
+      res.status(200).json(data.result);
+    })
+    .catch((err) => {
+      if (axios.isAxiosError(err)) res.status(500).json(err.message);
+      else res.status(500);
+    });
+})
 
 export default app;
